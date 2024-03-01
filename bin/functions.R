740baee48d6f85e20fbac14f0ab46c3f8549d0eb
Jack Scanlan <jacksca@gmail.com>
2024-03-01 12:09:27 +1100

    update

--- bin/functions.R
+++ bin/functions.R
@@ -1,8 +1,22 @@
 plot_dataset <- function(dataset) {
-    car_plot <- ggplot(dataset, aes(x = mpg, y = cyl, colour = disp)) +
-    geom_point() +
-    facet_grid(.~vs) +
-    ggtitle("Facet by 'vs'")
+    
+    if (?require(tidyverse)) {stop("'tidyverse' not installed!")} else {
 
-    ggsave("cars.pdf", plot = car_plot)
+        car_plot <- ggplot(dataset, aes(x = mpg, y = cyl, colour = disp)) +
+        geom_point() +
+        facet_grid(.~vs) +
+        ggtitle("Facet by 'vs'")
+
+        ggsave("cars.pdf", plot = car_plot, width = 5, height = 5)
+        
+        }
+
+}
+
+mutate_dataset <- function(dataset) {
+    
+    mtcars_new <-
+        mtcars %>% 
+        dplyr::mutate(mpg_div_cyl = mpg /cyl)
+
 }
