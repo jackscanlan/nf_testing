1a728c2f63d541719d6ca38d1fcdf9a777d45a83
Jack Scanlan <jacksca@gmail.com>
2024-03-01 12:15:07 +1100

    update

--- bin/functions.R
+++ bin/functions.R
@@ -1,20 +1,21 @@
-plot_dataset <- function(dataset) {
+plot_dataset <- function(dataset, x, y) {
     
     if (!require(tidyverse)) {stop("'tidyverse' not installed!")} else {
-        car_plot <- ggplot(dataset, aes(x = mpg, y = cyl, colour = disp)) +
+        car_plot <- ggplot(dataset, aes(x = x, y = y, colour = disp)) +
         geom_point() +
         facet_grid(.~vs) +
         ggtitle("Facet by 'vs'")
 
         ggsave("cars.pdf", plot = car_plot, width = 5, height = 5)
-        }
+    }
 
 }
 
 mutate_dataset <- function(dataset) {
     
+    if (!require(tidyverse)) {stop("'tidyverse' not installed!")} else {
     mtcars_new <-
-        mtcars %>% 
+        dataset %>% 
         dplyr::mutate(mpg_div_cyl = mpg /cyl)
 
 }
--- bin/myscript.R
+++ bin/myscript.R
@@ -1,4 +1,8 @@
 #!/usr/bin/R
 library(tidyverse)
 
-plot_dataset(mtcars)
+plot_dataset(mtcars, mpg, cyl)
+
+mutate_dataset(mtcars)
+
+plot_dataset(mtcars_new, mpg_div_cyl, cyl)
