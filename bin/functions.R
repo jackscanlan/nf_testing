e882cc14a5439bfef52a2da64e77fe1987f090af
Jack Scanlan <jacksca@gmail.com>
2024-03-01 12:00:33 +1100

    update

--- bin/functions.R
+++ bin/functions.R
@@ -1,7 +1,8 @@
 plot_dataset <- function(dataset) {
     car_plot <- ggplot(dataset, aes(x = mpg, y = cyl, colour = disp)) +
     geom_point() +
-    facet_grid(.~vs)
+    facet_grid(.~vs) +
+    ggtitle("Facet by 'vs'")
 
     ggsave("cars.pdf", plot = car_plot)
 }
