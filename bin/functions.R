ce1e6067bd5f97f03c8ba940fd9b7675da4f93f7
Jack Scanlan <jacksca@gmail.com>
2024-03-01 12:10:08 +1100

    update

--- bin/functions.R
+++ bin/functions.R
@@ -1,14 +1,13 @@
 plot_dataset <- function(dataset) {
     
-    if (?require(tidyverse)) {stop("'tidyverse' not installed!")} else {
-
+    if (?require(tidyverse)) {stop("'tidyverse' not installed!")} 
+    else {
         car_plot <- ggplot(dataset, aes(x = mpg, y = cyl, colour = disp)) +
         geom_point() +
         facet_grid(.~vs) +
         ggtitle("Facet by 'vs'")
 
         ggsave("cars.pdf", plot = car_plot, width = 5, height = 5)
-        
         }
 
 }
